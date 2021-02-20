eadcea49fcc8137f211c6af5760878d3185cfac9
Doniben <doniben@esperanto.co>
2021-02-20 18:39:38 -0500

    [FIX] Already connects db

--- dbConnection.php
+++ dbConnection.php
@@ -1,11 +1,10 @@
 <?php
-    $serverName = 'localhost';
+    /* $serverName = 'localhost';
     $connection = array("Database" => "prueba",
                         "UID" => "candidato",
                         "PWD" => "Candidato1!",
-                        "CharacterSet" => "UTF-8");
-    
-    $con = mysqli_connect($serverName, $connection);
+                        "CharacterSet" => "UTF-8"); */
+    $con = mysqli_connect('localhost', 'candidato', 'Candidato1!', 'prueba');
     if($con){
         echo "Base de datos conectada";
     } else{
